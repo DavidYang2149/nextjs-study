7ca5b791481bacb267b9d8270e366d13682730e9
davidyang2149 <davidyang2149@gmail.com>
2020-08-23 16:58:38 +0900

    [Add]nextJs dot-env-demo antd tailwindcss Add

--- dot-env-demo/pages/_app.js
+++ dot-env-demo/pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/index.css";
+//import "../styles/antd.less";
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
