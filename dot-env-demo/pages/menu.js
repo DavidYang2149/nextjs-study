9559425c06661cfea585bc59fb0b7e62c0a70adb
davidyang2149 <davidyang2149@gmail.com>
2020-08-23 18:31:56 +0900

    [Add]nextJs dot-env-demo api init Add

---
+++ dot-env-demo/pages/menu.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react";
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "메뉴",
+    dataIndex: "메뉴",
+    key: "메뉴",
+  },
+  {
+    title: "가격",
+    dataIndex: "가격",
+    key: "가격",
+  },
+];
+
+function Menu(props) {
+  return (
+    <Fragment>
+      <p>메뉴 페이지</p>
+      <Table dataSource={props.menu} columns={columns} rowKey={"메뉴"} />;
+    </Fragment>
+  );
+}
+
+Menu.getInitialProps = async () => {
+  const response = await fetch("http://127.0.0.1:4001/api/menu");
+  const menu = await response.json();
+  return {
+    menu,
+  };
+};
+
+export default Menu;
