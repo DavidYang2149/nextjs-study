b48f3f310c6c3c77a0b099dab36b9bb5703cba9c
davidyang2149 <davidyang2149@gmail.com>
2020-08-23 17:22:14 +0900

    [Add]nextJs dot-env-demo styled-components babelrc Add

--- dot-env-demo/pages/index.js
+++ dot-env-demo/pages/index.js
@@ -1,6 +1,18 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Button } from "antd";
+import styled from "styled-components";
+
+const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  height: 120px;
+  font-size: 14px;
+  border-radius: 50%;
+  border: 1px solid #e5e5e5;
+`;
 
 function Home() {
   return (
@@ -8,6 +20,11 @@ function Home() {
       <div>
         <h1>{process.env.APP_NAME}</h1>
         <p>API_HOST : {process.env.API_HOST}</p>
+        <div className="flex">
+          <Circle>1</Circle>
+          <Circle>2</Circle>
+          <Circle>3</Circle>
+        </div>
         <button className="btn-blue">Tailwind Button</button>
         <div>
           <Button>Welcome</Button>
