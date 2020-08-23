a81c6d0eeed6a3667d0048bcb13a052ecf7e9b9f
davidyang2149 <davidyang2149@gmail.com>
2020-08-23 22:04:51 +0900

    [Add]nextJs my-tunes init Add

---
+++ my-tunes/db/Album.js
@@ -0,0 +1,15 @@
+// Setting up the database connection
+const knex = require("knex")({
+  client: "sqlite",
+  connection: {
+    filename: "./db/my-tunes.db",
+  },
+});
+const bookshelf = require("bookshelf")(knex);
+
+// Defining models
+const Album = bookshelf.model("Album", {
+  tableName: "albums",
+});
+
+export default Album;
