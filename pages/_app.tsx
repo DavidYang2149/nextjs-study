c28f4787cf60b809d35aa9f7512c2a4db0ff3e9f
davidyang2149 <davidyang2149@gmail.com>
2021-10-12 20:51:02 +0900

    :hammer: App에 redux toolkit 적용
    nextjs study

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line import/extensions
 import React from 'react';
+import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
+
+import store from 'src/slices/store';
 
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
 
 export default MyApp;
