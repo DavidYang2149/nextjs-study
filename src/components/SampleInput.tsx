2bf95b0cf52b7f6984283fba490295ee3e7cf8c6
davidyang2149 <davidyang2149@gmail.com>
2021-10-19 21:01:58 +0900

    :hammer: 샘플 입력창을 제작하라
    nextjs study

---
+++ src/components/SampleInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SampleInput = () => {
+  return (
+    <div>
+      <label htmlFor="sample-input">입력</label>
+      <input type="text" id="sample-input" />
+    </div>
+  )
+};
+
+export default SampleInput;
