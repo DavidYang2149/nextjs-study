cd143cdc4816c3b14fa2ed787823f5dd6840bec3
davidyang2149 <davidyang2149@gmail.com>
2021-10-20 19:46:51 +0900

    :hammer: 리덕스 값을 input에 적용하라
    nextjs study

--- src/components/SampleInput.tsx
+++ src/components/SampleInput.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const SampleInput = () => {
+export interface Props {
+  value: string;
+}
+
+const SampleInput = ({ value }: Props) => {
   return (
     <div>
       <label htmlFor="sample-input">입력</label>
-      <input type="text" id="sample-input" />
+      <input type="text" id="sample-input" value={value} />
     </div>
   )
 };
--- src/containers/SampleContainer.tsx
+++ src/containers/SampleContainer.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import SampleInput from 'src/components/SampleInput';
 
 import SampleItem from 'src/components/SampleItem';
+import { RootState } from 'src/slices/rootReducer';
 
 const SampleContainer = () => {
+  const { sample } = useSelector((state: RootState) => ({
+    sample: state.sample,
+  }));
+  const value = sample.sample;
   return (
     <>
       <SampleItem />
-      <SampleInput />
+      <SampleInput value={value} />
     </>
   )
 };
